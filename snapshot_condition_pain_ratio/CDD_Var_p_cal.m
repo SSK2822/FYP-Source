d04f20c41a2c3a3a23b5c759a17f753514535e6f
Shreyash Kawle <77000727+SSK2822@users.noreply.github.com>
2023-07-12 14:27:55 -0700

    Updates Con Pain

--- snapshot_condition_pain_ratio/CDD_Var_p_cal.m
+++ snapshot_condition_pain_ratio/CDD_Var_p_cal.m
@@ -6,8 +6,6 @@ options = optimoptions('fmincon','Algorithm','sqp', 'MaxFunctionEvaluations',MyV
     'ConstraintTolerance',1e-6,'MaxIterations',800,'StepTolerance',1e-8);
 [~, CDD_VaR0, a] = fmincon(@(rho) CDD_Var_p(weeks, wk_return_d1, xt_all, rho, theta), 0.1, [], [], [], [], 0, [], [], options);
 
-%[~, CDD_VaR0, a] = fmincon(@(rho) EVaR_p(weeks, wk_return_d1, xt_all, rho, theta), 0.01, [], [], [], [], 0, [], []);
-
 if(a ~= 1)
     disp(['p_cal:' num2str(a)])
 end
